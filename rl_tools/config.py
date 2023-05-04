0e193c30c2f2771d5d47fd69703ccc90ca1d0a07
Raffaelbdl <raffaelbdl@gmail.com>
2023-05-04 09:22:13 +0900

    🎉 Add read_config

---
+++ rl_tools/config.py
@@ -0,0 +1,12 @@
+import yaml
+from yaml.loader import SafeLoader
+
+
+def get_config(config_path: str, kwargs):
+    with open(config_path) as f:
+        config: dict = yaml.load(f, SafeLoader)
+
+    for k, v in kwargs.items():
+        config[k] = v
+
+    return config
