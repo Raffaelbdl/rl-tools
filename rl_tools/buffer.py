9311a1a5343f89fe9c6d2e23cdc7599e8d29acd0
Raffaelbdl <raffaelbdl@gmail.com>
2023-05-08 08:59:46 +0900

    ⚡ Add replaybuffer

--- rl_tools/buffer.py
+++ rl_tools/buffer.py
@@ -1,5 +1,8 @@
 from abc import abstractmethod
-from typing import List
+from typing import Dict, List
+
+from gymnasium.spaces import Space
+import numpy as np
 
 
 class Buffer:
@@ -87,3 +90,75 @@ class SimpleOnPolicyBuffer(Buffer):
         for d in data:
             batch.append(SimpleOnPolicyBuffer.get_time_batch_from_data(d, idx))
         return batch
+
+
+
+class ReplayBuffer:
+    """Buffer for off-policy algorithms"""
+
+    def __init__(
+        self,
+        seed: int,
+        buffer_size: int,
+        obs_space: Space,
+        action_space: Space,
+    ) -> None:
+        self.rng = np.random.default_rng(seed=seed)
+        self.buffer_size = max(buffer_size, 1)
+
+        self.obs_shape = obs_space.shape
+        self.action_shape = action_space.shape
+
+        self.observations = np.zeros(
+            (self.buffer_size,) + self.obs_shape, obs_space.dtype
+        )
+        self.actions = np.zeros(
+            (self.buffer_size,) + self.action_shape, action_space.dtype
+        )
+        self.rewards = np.zeros((self.buffer_size,), np.float32)
+        self.dones = np.zeros((self.buffer_size,), np.float32)
+
+        self.pos = 0
+        self.full = False
+
+    def add(
+        self,
+        obs: np.ndarray,
+        action: np.ndarray,
+        reward: np.ndarray,
+        done: np.ndarray,
+        next_obs: np.ndarray,
+    ) -> None:
+        obs = obs.reshape(self.obs_shape)
+        action = action.reshape(self.action_shape)
+        reward = reward
+        done = done
+        next_obs = obs.reshape(self.obs_shape)
+
+        self.observations[self.pos] = obs.copy()
+        self.actions[self.pos] = action.copy()
+        self.rewards[self.pos] = reward
+        self.dones[self.pos] = done
+        self.observations[(self.pos + 1) % self.buffer_size] = next_obs.copy()
+
+        self.pos += 1
+        if self.pos == self.buffer_size:
+            self.full = True
+            self.pos = 0
+
+    def sample(self, batch_size: int):
+        if self.full:
+            inds = self.rng.integers(1, self.buffer_size, size=batch_size)
+            inds = (inds + self.pos) % self.buffer_size
+        else:
+            inds = self.rng.integers(0, self.pos, size=batch_size)
+        return self._get_samples(inds)
+
+    def _get_samples(self, inds: np.ndarray) -> Dict[str, np.ndarray]:
+        return {
+            "observations": self.observations[inds],
+            "actions": self.actions[inds],
+            "rewards": self.rewards[inds],
+            "dones": self.dones[inds],
+            "next_observations": self.observations[(inds + 1) % self.buffer_size],
+        }
